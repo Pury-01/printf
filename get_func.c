b21fbec7fbabe8ca72fbb7b89d0455d05e6cde0d
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-10-15 21:40:40 +0300

    fetches the right function for the corresponding format specifier

---
+++ get_func.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_func - fetches the right function for the corresponding specifier
+ * @specifier: the palce holder for a given datatype
+ * Return: the corresponding function for a specifier
+ */
+
+char (*get_func(char specifier))(va_list args)
+{
+	if (specifier == 'c')
+	{
+		return (c_handler);
+	}
+	if (specifier == 's')
+	{
+		return (s_handler);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
