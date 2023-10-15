ca245c5bfada56f5ae6b8d6f535374faeacaf42e
caleb-kibe <kiplagatcaleb2@gmail.com>
2023-10-15 21:39:42 +0300

    our printf-like function

---
+++ my_printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _printf - prints a format string to the standard output
+ * @format: string to be printed
+ * Return: nmuber of characters primted
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c_handler(args);
+				count++;
+			}
+			if (*format == 's')
+			{
+				s_handler(args);
+				count++;
+			}
+			if (*format == '%')
+			{
+				write(1, &%%, 2);
+				count++;
+			}
+		}
+		else
+			write(1, format, 1);
+		count++;
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
