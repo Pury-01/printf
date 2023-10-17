94960fd456ff2e26649358b5615a502ae7482940
Pury-01 <puryokumu@gmail.com>
2023-10-17 07:10:44 +0300

    Header file

--- main.h
+++ main.h
@@ -9,6 +9,6 @@
 int _printf(const char *format, ...);
 void c_handler(va_list args);
 void s_handler(va_list args);
-char(*get_func(char specifier))(va_list args);
+void(*get_func(char specifier))(va_list args);
 
 #endif /* MAIN_H */
